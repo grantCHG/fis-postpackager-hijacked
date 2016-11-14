afbc4ef31274885fd1407cb2b7386a8145f5132d
梁程广 <liangchengguang@kugou.net>
2016-11-14 13:58:45 +0800

    white list

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
  'use strict';
  module.exports = function(ret, file, settings) {
+
  	let jscore = '<script src="http://static.kgimg.com/common/js/min/hijacked-min.js"></script>';
- 	let ignoreFileReg = settings.ignoreFile || '';
+ 	let ignoreFileReg = settings.ignoreFile || '',
+ 		whiteList = settings.whiteList || '';
  	if (ignoreFileReg instanceof Array) {
  		ignoreFileReg = ignoreFileReg.join("|");
  	}
 
  	if (ignoreFileReg.length > 0) {
  		ignoreFileReg = new RegExp(ignoreFileReg, "gi");
+ 	}
+ 	if (whiteList instanceof Array) {
+ 		jscore = '<script>window["HIJACKED_LIST"]=' + JSON.stringify(whiteList) + ';</script><script src="http://static.kgimg.com/common/js/min/hijacked-min.js"></script>';
  	}
  	fis.util.map(ret.src, function(subpath, file, i) {
  		if (file.isHtmlLike && !(ignoreFileReg && file.origin.match(ignoreFileReg))) {
